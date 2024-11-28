21e1cadc9244ebb44f4b23639f3b6c2d9f9b7deb
Ryan d'Huart <homoelectromagneticus@protonmail.com>
2024-11-27 21:55:06 -0500

    Basic tokenizing started
    A very raw structure is there
    Some promising early results on a sample text
    So far only the only things really parsed are standard text, bold,
    italics, and newlines

--- src/main.zig
+++ src/main.zig
@@ -1,64 +1,108 @@
 const std = @import("std");
 
-fn handleHeaderLine(markdown_line: []const u8, html: *std.ArrayListAligned(u8,null)) !void {
-    // count the number of '#' at the beginning of the line
-    var header_level: u8 = 0;
-    for (markdown_line) |character| {
-        if (character == '#') {
-            header_level += 1;
-        }
+// the types of possible markdown objects i care about tokenizing
+const Type = enum {
+    newline,
+    text,
+    header,
+    bold,
+    italics,
+    linebreak,
+    link,
+    image,
+};
+
+// a markdown node
+const Node = struct {
+    kind: Type,
+    level: u8 = 0,
+    resource: []const u8 = "",
+
+    fn init_text(raw_text: []const u8) Node {
+        return Node{
+            .kind = Type.text,
+            .level = 0,
+            .resource = raw_text,
+        };
     }
-    // use the calculated header level to build the appropriate html tags 
-    try html.*.writer().print("<h{d}>", .{header_level}); 
-    try html.*.appendSlice(markdown_line[header_level..]);
-    try html.*.writer().print("</h{d}>", .{header_level}); 
+};
+
+fn appendTextNode(raw_text: []const u8, nodes: *std.ArrayListAligned(Node, null)) !void {
+    try nodes.append(.{
+        .kind = Type.text,
+        .level = 0,
+        .resource = raw_text,
+    });
 }
 
-test "first-level header" {
-    const markdown: []const u8 = "#Header 1";
-    var test_html = std.ArrayList(u8).init(std.testing.allocator);
-    defer test_html.deinit();
-    try handleHeaderLine(markdown, &test_html);
-    try std.testing.expectEqualStrings("<h1>Header 1</h1>", test_html.items);
-}
-
-test "third-level header" {
-    const markdown: []const u8 = "###Header 3";
-    var test_html = std.ArrayList(u8).init(std.testing.allocator);
-    defer test_html.deinit();
-    try handleHeaderLine(markdown, &test_html);
-    try std.testing.expectEqualStrings("<h3>Header 3</h3>", test_html.items);
-}
-
-fn parseMarkdown(markdown: []const u8, html: *std.ArrayListAligned(u8,null)) !void {
+fn parseMarkdown(markdown: []const u8, nodes: *std.ArrayListAligned(Node, null)) !void {
     // handle empty input
     if (markdown.len == 0) return; 
-    
-    // split the markdown input on newlines
-    var markdown_split = std.mem.splitSequence(u8, markdown, "\n");
 
-    // we need to know if we are inside a paragraph
-    while (markdown_split.next()) |line| {
-        // nothing to do on empty lines (for now...)
-        if (std.mem.eql(u8, line, "\n") or line.len == 0) {
-            try html.*.append('\n');
+    var kind: Type = Type.text;
+    var header_depth: u8 = 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var text_buffer = std.ArrayList(u8).init(gpa.allocator());
+    defer text_buffer.deinit();
+
+    // go through the source character-by-character
+    for (markdown, 0..) |character, index| {
+        // BOLD AND ITALICS
+        // if we hit a '*' or a '_', we need to insert either an italics node
+        // or a bold node depending on the previous character
+        if (character == '*' or character == '_') {
+            // if the '*' or the '_' is repeated, then we need to remove the 
+            // italics node we've made just before and add a bold node
+            if (character == markdown[index - 1]) {
+                _ = nodes.pop();
+                try nodes.append(.{
+                    .kind = Type.bold,
+                    .level = 0,
+                    .resource = "",
+                });
+            // if it's not repeated, then we save all the text up until now 
+            // (if there is any) and create an italics node
+            } else {
+                if (text_buffer.items.len > 0) {
+                    try nodes.append(Node.init_text(try text_buffer.toOwnedSlice()));
+                }
+                try nodes.append(.{
+                    .kind = Type.italics,
+                    .level = 0,
+                    .resource = "",
+                });
+            }
             continue;
         }
-        // check and handle headers
-        if (line[0] == '#') {
-            try handleHeaderLine(line, html);
+
+        // NEWLINE
+        // if we hit a newline, then we need to make the right node
+        if (character == '\n') {
+            if (text_buffer.items.len > 0) {
+               try nodes.append(Node.init_text(try text_buffer.toOwnedSlice()));
+            }
+            // and we need to append a newline node
+            try nodes.append(.{
+                .kind = Type.newline,
+                .level = 0,
+                .resource = "",
+            });
+            // reset the parameters for the next node (we don't need to do this
+            // for the text, since `toOwnedSlice()` takes care of that)
+            kind = Type.text;
+            header_depth = 0;
+            // move on to the next character
             continue;
         }
-        try html.*.appendSlice(line);
-        try html.*.append('\n');
-    } else {
-        // remove trailing newline generated by the above loop
-        _ = html.*.pop();
-    }
+
+        // REGULAR TEXT
+        try text_buffer.append(character);
+    }    
 }
 
 test "empty input" {
-    var test_html = std.ArrayList(u8).init(std.testing.allocator);
+    var test_html = std.ArrayList(Node).init(std.testing.allocator);
     defer test_html.deinit();
     try parseMarkdown("", &test_html);
     try std.testing.expectEqual(0, test_html.items.len);
@@ -90,18 +134,17 @@ pub fn main() !void {
     // print a copy of the markdown for debugging
     try stdout.print("Markdown input:\n{s}", .{markdown});
 
-    // we'll need to allocate more memory to build the output html content
-    var html = std.ArrayList(u8).init(gpa.allocator());
-    defer html.deinit();
-  
-    // add the opening html tags, parse the markdown, and then add the closing
-    // html tags (with newlines so it's more readable)
-    try html.appendSlice("<html>\n");
-    try parseMarkdown(markdown, &html); 
-    try html.appendSlice("\n</html>");
+    // we'll need to allocate more memory to build the list of markdown nodes
+    var nodes = std.ArrayList(Node).init(gpa.allocator());
+    defer nodes.deinit();
 
-    // print the html for debugging
-    try stdout.print("\nHTML output:\n{s}\n", .{html.items});
+    // parse markdown into nodes
+    try parseMarkdown(markdown, &nodes);
+
+    for (nodes.items) |node| {
+        if (node.kind == Type.newline) try stdout.print("\n", .{});
+        try stdout.print("{s} ", .{@tagName(node.kind)});
+    }
     
     try bw.flush(); // don't forget to flush!
 }
